5af3ded5c8332a2987ae35e2f2ee372453ad567b
Guido <fullimmersion@MacBook-Air-di-Sara.local>
2025-10-05 19:52:04 +0200

    chore(types): declare bcryptjs module for build

---
+++ src/types/bcryptjs.d.ts
@@ -0,0 +1,17 @@
+declare module 'bcryptjs' {
+  export function hash(data: string, saltOrRounds: string | number): Promise<string>;
+  export function hashSync(data: string, saltOrRounds: string | number): string;
+  export function compare(data: string, encrypted: string): Promise<boolean>;
+  export function compareSync(data: string, encrypted: string): boolean;
+  export function genSalt(rounds?: number): Promise<string>;
+  export function genSaltSync(rounds?: number): string;
+  const _default: {
+    hash: typeof hash;
+    hashSync: typeof hashSync;
+    compare: typeof compare;
+    compareSync: typeof compareSync;
+    genSalt: typeof genSalt;
+    genSaltSync: typeof genSaltSync;
+  };
+  export default _default;
+}
